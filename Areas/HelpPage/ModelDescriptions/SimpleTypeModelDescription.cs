a31d5c735e29f6e63f0b622aaa3fc214ded6c946
thaivo <thai2903@gmail.com>
2021-02-10 13:34:46 -0500

    Add project files.

---
+++ Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace n01458860Assignment2.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="n01458860Assignment2.WebApiApplication" Language="C#" %>
