ae53c185e082bfdeae6bb399cc0333ab6121dadc
thaivo <thai2903@gmail.com>
2021-02-10 15:14:16 -0500

    Solve problem 1 and 2

---
+++ Controllers/J1Controller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace n01458860Assignment2.Controllers
+{
+    public class J1Controller : ApiController
+    {
+        private int[] burgers = { 461, 431, 420, 0 };
+        private int[] drinks = { 130, 160, 118, 0 };
+        private int[] sides = { 100, 57, 70, 0 };
+        private int[] desserts = { 167, 266, 75, 0 };
+
+        /// <summary>
+        /// Compute the total Calories of the meal
+        /// and result string include the total
+        /// </summary>
+        /// <param name="burger">index of burger</param>
+        /// <param name="drink">index of drink</param>
+        /// <param name="side">index of side</param>
+        /// <param name="dessert">index of dessert</param>
+        /// <returns> Message show the total Calories of the meal</returns>
+        /// <example>
+        /// GET ../api/J1/Menu/1/2/3/4
+        /// -> Response is "Your total calorie count is 691"
+        /// </example>
+        [HttpGet]
+        [Route("api/j1/menu/{burger}/{drink}/{side}/{dessert}")]
+        public string Menu(int burger, int drink, int side, int dessert)
+        {
+            string result = "Your total calorie count is ";
+            int total = burgers[burger-1] + drinks[drink-1] + sides[side-1] + desserts[dessert-1];
+            return result + total.ToString();
+        }
+    }
+}
---
+++ Controllers/J2Controller.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace n01458860Assignment2.Controllers
+{
+    public class J2Controller : ApiController
+    {
+        private const string message1 = "There are ";
+        private const string message2 = " ways to get the sum 10.";
+        /// <summary>
+        /// Calculate the number of way for 2 dice to get the total of 10
+        /// </summary>
+        /// <param name="m"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("api/J2/DiceGame/{m}/{n}")]
+        public string DiceGame(int m, int n)
+        {
+            string result = "";
+            int way = m + n - 9;
+            int max = m;
+            int min = n;
+            if (m <= n)
+            {
+                max = n;
+                min = m;
+            }
+            
+            if (way < 1)
+            {
+                result = message1 + "0" + message2;
+            }
+            else if (way < min && way < max)
+            {
+                result = message1 + way.ToString() + message2;
+            }
+            else if(way > min )
+            {
+                result = message1 + min.ToString() + message2;
+            }
+            return result;
+        }
+    }
+}
